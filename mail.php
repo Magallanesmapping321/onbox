afb48e05d1156f0ceabcea7d87a30f6a3d50589b
pensus <83836512+Magallanesmapping321@users.noreply.github.com>
2021-07-17 15:34:30 -0400

    There is no messages now go play fortnite

---
+++ mail.php
@@ -0,0 +1,11 @@
+<?php
+require('config.php');
+?>
+<title>Mail - <?php echo $siteTitle; ?></title>
+<h1><?php echo $siteTitle; ?> Mail</h1>
+<p>Current account: <?php echo $usermail; ?></p>
+<hr><center>Messages | <a href="compose.php">Compose message</a></center>
+<hr>
+Latest Messages | <a href="compose.php">Compose message</a><br><hr>
+<a href="1.php"><?php echo $siteTitle; ?>                             | Welcome to <?php echo $siteTitle; ?>!</a>
+<hr>
