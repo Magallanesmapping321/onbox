c5ad15922fa1f795f1f3218522be953508306868
pensus <83836512+Magallanesmapping321@users.noreply.github.com>
2021-07-17 15:33:20 -0400

    type peter griffin

---
+++ create.php
@@ -0,0 +1,12 @@
+<?php
+require('config.php');
+?>
+<title><?php echo $siteTitle; ?> sign up</title>
+<center><br><h1><?php echo $siteTitle; ?> sign up</h1>
+<br>
+<input type="text" placeholder="<?php echo $usermail; ?> " disabled><br>
+<input type="password" placeholder="type <?php echo $usertype; ?>"><br>
+<a href="login.php">create</a><br>
+<a href="index.php">back</a>
+
+</center>
