732cfec5195c08b178d99e8408a433e92695d247
pensus <83836512+Magallanesmapping321@users.noreply.github.com>
2021-07-17 15:48:52 -0400

    haha update go brrrrrrrrrrrrrrr

--- index.php
+++ index.php
@@ -2,4 +2,6 @@
 require('config.php');
 ?>
 <title><?php echo $siteTitle; ?></title>
-<center><h1><?php echo $siteWelcome; ?></h1><p><?php echo $uselessDesc; ?></p><a href="create.php">Create mail</a><br><a href="login.php">Sign in</a><br><img src="https://i.ytimg.com/vi/d34GkgZ14X4/sddefault.jpg"></center>
+<center><h1><?php echo $siteWelcome; ?></h1><p><?php echo $uselessDesc; ?></p><a href="create.php">Create mail</a><br><a href="login.php">Sign in</a>
+  <!--- funny image --->
+  <br><img src="https://i.ytimg.com/vi/d34GkgZ14X4/sddefault.jpg"></center>
