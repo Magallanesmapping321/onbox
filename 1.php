ef4c39e9883dad3191c11165b67efdcb36557ec8
pensus <83836512+Magallanesmapping321@users.noreply.github.com>
2021-07-17 15:35:34 -0400

    el pepe

---
+++ 1.php
@@ -0,0 +1,7 @@
+<?php
+require('config.php');
+?>
+<title><?php echo $siteTitle; ?> - Welcome to <?php echo $siteTitle; ?>!</title>
+<h1>Welcome to <?php echo $siteTitle; ?>!</h1>
+<p><?php echo $siteTitle; ?> is a mail service that doesnt work at all xD</p>
+<a href="mail.php">Back to messages</a>
