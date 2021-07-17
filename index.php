dfbb8dd181167f2e6fa7b5c0702d2dfd9168183b
pensus <83836512+Magallanesmapping321@users.noreply.github.com>
2021-07-17 15:31:40 -0400

    HAHAHAHHAHHA I AM SUS

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+require('config.php');
+?>
+<title><?php echo $siteTitle; ?></title>
+<center><h1><?php echo $siteWelcome; ?></h1><p><?php echo $uselessDesc; ?></p><a href="create.php">Create mail</a><br><a href="login.php">Sign in</a><br><img src="https://i.ytimg.com/vi/d34GkgZ14X4/sddefault.jpg"></center>
