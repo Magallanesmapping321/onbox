25f21ed55728b01fb1ed818d61ec3f8e7e1d5383
pensus <83836512+Magallanesmapping321@users.noreply.github.com>
2021-07-17 15:32:26 -0400

    configurations because yes

---
+++ config.php
@@ -0,0 +1,21 @@
+<?php
+/* Onbox - The worst email software */
+
+$siteTitle = 'Onbox';
+/* title of your gmail clone */
+
+$siteWelcome = 'Welcome to Onbox';
+/* the welcome sign */
+
+$uselessDesc = 'Onbox is a funny email service';
+/* description */
+
+$usermail = 'poopoopeepeefard69@elpe.pe';
+/* mail of the user */
+
+$usertype = 'peter griffin';
+/* passworte of the user */
+
+$compose = 'POOPOO PEEPEE I STINK SADLY'; 
+/* default compose message (leave blank if gigachad)*/
+?>
