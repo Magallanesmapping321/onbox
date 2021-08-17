4c2776dac616d3b5069759dec0aa3da5867275fb
pensus <83836512+Magallanesmapping321@users.noreply.github.com>
2021-08-17 15:32:19 -0400

    Update 1.php

--- 1.php
+++ 1.php
@@ -5,4 +5,5 @@ require('config.php');
 <title><?php echo $siteTitle; ?> - Welcome to <?php echo $siteTitle; ?>!</title>
 <h1>Welcome to <?php echo $siteTitle; ?>!</h1>
 <p><?php echo $siteTitle; ?> is a mail service that doesnt work at all xD</p>
+<small><b>by <?php echo $siteTitle; ?></b></small><br>
 <a href="mail.php">Back to messages</a>
