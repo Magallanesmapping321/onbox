8a32279ce941d4c45e24bb19d05db6941dabb630
pensus <83836512+Magallanesmapping321@users.noreply.github.com>
2021-08-17 15:24:47 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 require('config.php');
 ?>
+<link rel="stylesheet" href="https://getbootstrap.com/docs/5.1/dist/css/bootstrap.min.css">
 <title><?php echo $siteTitle; ?></title>
 <center><h1><?php echo $siteWelcome; ?></h1><p><?php echo $uselessDesc; ?></p><a href="create.php">Create mail</a><br><a href="login.php">Sign in</a>
   <!--- funny image --->
