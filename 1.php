e2a4bf910c44cb33e50efd6f08d6a7569f468fd4
pensus <83836512+Magallanesmapping321@users.noreply.github.com>
2021-08-17 15:23:51 -0400

    Update 1.php

--- 1.php
+++ 1.php
@@ -1,6 +1,7 @@
 <?php
 require('config.php');
 ?>
+<link rel="stylesheet" href="https://getbootstrap.com/docs/5.1/dist/css/bootstrap.min.css">
 <title><?php echo $siteTitle; ?> - Welcome to <?php echo $siteTitle; ?>!</title>
 <h1>Welcome to <?php echo $siteTitle; ?>!</h1>
 <p><?php echo $siteTitle; ?> is a mail service that doesnt work at all xD</p>
